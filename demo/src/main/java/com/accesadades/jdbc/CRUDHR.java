7d8ef91e519ea55764a3960bb08594a6dd440f17
A.T. <atalens1@xtec.cat>
2024-11-28 02:57:25 +0100

    version 4

---
+++ demo/src/main/java/com/accesadades/jdbc/CRUDHR.java
@@ -0,0 +1,106 @@
+package com.accesadades.jdbc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ConnectException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CRUDHR {
+    
+    public void CreateDatabase(Connection connection, InputStream input) 
+    throws IOException, ConnectException, SQLException {
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(input))) {
+            StringBuilder sqlStatement = new StringBuilder();
+            String line;
+
+            try (Statement statement = connection.createStatement()) {
+                while ((line = br.readLine()) != null) {
+                // Ignorar comentaris i línies buides
+                    line = line.trim();
+                        
+                    if (line.isEmpty() || line.startsWith("--") || line.startsWith("//") || line.startsWith("/*")) {
+                            continue;
+                    }
+
+                    // Acumular la línea al buffer
+                    sqlStatement.append(line);
+
+                    // el caràcter ";" es considera terminació de sentència SQL
+                    if (line.endsWith(";")) {
+                    // Eliminar el ";" i executar la instrucción
+                        String sql = sqlStatement.toString().replace(";", "").trim();
+                        statement.execute(sql);
+
+                        // Reiniciar el buffer para la siguiente instrucción
+                        sqlStatement.setLength(0);
+                    }
+                }
+            } 
+        }
+    }
+
+//Read sense prepared statements, mostra tots els registres
+    public void ReadDatabase(Connection connection, String TableName) throws ConnectException, SQLException {
+        try (Statement statement = connection.createStatement()) {
+            
+            String query = "SELECT * FROM " + TableName + ";";
+
+            ResultSet rset = statement.executeQuery(query);
+            
+            //obtenim numero de columnes i nom
+            int colNum = getColumnNames(rset);
+
+            //Si el nombre de columnes és >0 procedim a llegir i mostrar els registres
+            if (colNum > 0) {
+
+                recorrerRegistres(rset,colNum);
+
+            }
+        }
+    }
+
+
+//Aquest mètode auxiliar podria ser utileria del READ, mostra el nom de les columnes i quantes n'hi ha
+    public static int getColumnNames(ResultSet rs) throws SQLException {
+        
+        int numberOfColumns = 0;
+        
+        if (rs != null) {   
+            ResultSetMetaData rsMetaData = rs.getMetaData();
+            numberOfColumns = rsMetaData.getColumnCount();   
+        
+            for (int i = 1; i < numberOfColumns + 1; i++) {  
+                String columnName = rsMetaData.getColumnName(i);
+                System.out.print(columnName + ", ");
+            }
+        }
+        
+        System.out.println();
+
+        return numberOfColumns;
+        
+    }
+
+    public void recorrerRegistres(ResultSet rs, int ColNum) throws SQLException {
+
+        while(rs.next()) {
+            for(int i =0; i<ColNum; i++) {
+                if(i+1 == ColNum) {
+                    System.out.println(rs.getString(i+1));
+                } else {
+            
+                System.out.print(rs.getString(i+1)+ ", ");
+                }
+            } 
+        }
+            
+    }
+        
+}
--- demo/src/main/java/com/accesadades/jdbc/GestioDBHR.java
+++ demo/src/main/java/com/accesadades/jdbc/GestioDBHR.java
@@ -1,72 +1,147 @@
 package com.accesadades.jdbc;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class GestioDBHR {
     public static void main(String[] args) {
-        try {
-            // Ruta de l'arxiu de configuració
-            String configFilePath = "C:\\Users\\46456715G\\M06-UF2-JDBC-HR\\demo\\src\\main\\resources\\config.properties";
 
-            // Carregar propietats des de l'arxiu
-            Properties properties = new Properties();
-            try (FileInputStream input = new FileInputStream(configFilePath)) {
-                properties.load(input);
-            }
+        try (BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in))) {
 
-            // Obtenir les credencials com a part del fitxer de propietats
-            String dbUrl = properties.getProperty("db.url");
-            String dbUser = properties.getProperty("db.username");
-            String dbPassword = properties.getProperty("db.password");
+            try {
+                // Carregar propietats des de l'arxiu
+                Properties properties = new Properties();
+                try (InputStream input = GestioDBHR.class.getClassLoader().getResourceAsStream("config.properties")) {
+                //try (FileInputStream input = new FileInputStream(configFilePath)) {
+                    properties.load(input);
+    
+                    // Obtenir les credencials com a part del fitxer de propietats
+                    String dbUrl = properties.getProperty("db.url");
+                    String dbUser = properties.getProperty("db.username");
+                    String dbPassword = properties.getProperty("db.password");
+    
+                    // Conectar amb MariaDB
+                    try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+                        System.out.println("Conexió exitosa");
+    
+                        String File_create_script = "db_scripts/DB_Schema_HR.sql" ;
+    
+                        InputStream input_sch = GestioDBHR.class.getClassLoader().getResourceAsStream(File_create_script);
+    
+                        CRUDHR crudbhr = new CRUDHR();
+                        //Aquí farem la creació de la database i de les taules, a més d'inserir dades
+                        crudbhr.CreateDatabase(connection,input_sch);
 
-            // Conectar amb MariaDB
-            try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
-                System.out.println("Conexió exitosa");
-                String File_script = "C:\\Users\\46456715G\\M06-UF2-JDBC-HR\\demo\\db_scripts\\MySQL_Schema_HR.sql" ;
-
-                try (BufferedReader br = new BufferedReader(new FileReader(File_script))) {
-                    StringBuilder sqlStatement = new StringBuilder();
-                    String line;
-
-                    try (Statement statement = connection.createStatement()) {
-                        while ((line = br.readLine()) != null) {
-                        // Ignorar comentaris i línies buides
-                        line = line.trim();
-                        
-                            if (line.isEmpty() || line.startsWith("--") || line.startsWith("//") 
-                         || line.startsWith("/*")) {
-                            continue;
-                            }
-
-                            // Acumular la línea al buffer
-                            sqlStatement.append(line);
-
-                            // ";" es considera terminació de sentència SQL
-                            if (line.endsWith(";")) {
-                            // Eliminar ";" i executar la instrucción
-                                String sql = sqlStatement.toString().replace(";", "").trim();
-                                statement.execute(sql);
-
-                            // Reiniciar el buffer para la siguiente instrucción
-                                sqlStatement.setLength(0);
-                            }
-                        }
+                        MenuOptions(br1,crudbhr,connection);
+    
+                    } catch (Exception e) {
+                        System.err.println("Error al conectar: " + e.getMessage());
                     }
+                } catch (Exception e) {
+                    System.err.println("Error al carregar fitxer de propietats: " + e.getMessage());
                 }
-
-            }
+            } finally {}
             
         } catch (IOException e) {
-            System.err.println("Error al carregar l'arxiu de configuració: " + e.getMessage());
-        } catch (Exception e) {
-            System.err.println("Error al conectar: " + e.getMessage());
+            e.printStackTrace();
         }
+    }
+
+    public static void MenuOptions(BufferedReader br, CRUDHR crudbhr, Connection connection) 
+    throws NumberFormatException, IOException, SQLException {
+
+        System.out.println("CONSULTA BD HR");
+        System.out.println("OPCIONS");
+        System.out.println("1. CARREGAR TAULA");
+        System.out.println("2. CONSULTAR DADES");
+
+        System.out.print("Introdueix l'opció tot seguit >>");
+
+        int opcio = Integer.parseInt(br.readLine());
+
+        switch(opcio) {
+            case 1:
+                String File_data_script = "db_scripts/DB_Data_HR.sql" ;
+    
+                InputStream input_data = GestioDBHR.class.getClassLoader().getResourceAsStream(File_data_script);
+
+                crudbhr.CreateDatabase(connection,input_data);
+
+                break;
+            case 2:
+                //Preguntem de quina taula volem mostrar
+                MenuSelect(br,crudbhr,connection);
+                break;
+
+            default:
+                System.out.print("Opcio no vàlida");
+                MenuOptions(br,crudbhr,connection);
+        }
+    
+    }
+
+    public static void MenuSelect(BufferedReader br, CRUDHR crudbhr,Connection connection) 
+    throws SQLException, NumberFormatException, IOException {
+
+        int opcio = 0;
+
+        System.out.println("De quina taula vols mostrar els seus registres?");
+        System.out.println("1. Departaments");
+        System.out.println("2. Tasques");
+        System.out.println("3. Històric de tasques");
+        System.out.println("4. Empleats");
+        System.out.println("5. Sortir");
+
+        System.out.print("Introdueix l'opció tot seguit >>");
+
+        opcio = Integer.parseInt(br.readLine());
+
+        boolean sortir = true;
+
+        while (sortir != false) {
+            System.out.println("sortir: " + sortir);
+            switch(opcio) {
+                case 1:
+                    crudbhr.ReadDatabase(connection, "DEPARTMENTS");
+                    break;
+                case 2:
+                    crudbhr.ReadDatabase(connection, "JOBS");
+                    break;
+                case 3:
+                    crudbhr.ReadDatabase(connection, "JOB_HISTORY");
+                    break;
+                case 4:
+                    crudbhr.ReadDatabase(connection, "EMPLOYEES");
+                    break;
+                case 5:
+                    sortir = false;
+                    break;
+                default:
+                    System.out.print("Opcio no vàlida");
+                    MenuSelect(br,crudbhr, connection);
+            }
+
+            System.out.println("Vols fer altra consulta? (S o N): ");
+            String opcioB = br.readLine();
+            if (opcioB.equals("N")){
+                System.out.print("NO NO NO");
+                opcio = 5;
+                sortir = false;
+                break;
+            } else {
+                System.out.print("aqui");
+                MenuSelect(br,crudbhr, connection);
+            }
+
+            System.out.println("sortir - B: " + sortir);
+
+        }
+
     }
 }
